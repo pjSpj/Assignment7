31f2698372d51219346903b04a33cd40483ad186
upstairskid <45738525+upstairskid@users.noreply.github.com>
2019-03-07 18:47:27 -0500

    version2

--- assets/js.js
+++ assets/js.js
@@ -1,31 +1,54 @@
 var config = {
-    apiKey: "AIzaSyC80JR4C31POctSvZGDaupMPeYszcvs7zw",
-    authDomain: "train-c0284.firebaseapp.com",
-    databaseURL: "https://train-c0284.firebaseio.com",
-    projectId: "train-c0284",
-    storageBucket: "train-c0284.appspot.com",
-    messagingSenderId: "249191595233"
-  };
-  firebase.initializeApp(config);
+  apiKey: "AIzaSyC80JR4C31POctSvZGDaupMPeYszcvs7zw",
+  authDomain: "train-c0284.firebaseapp.com",
+  databaseURL: "https://train-c0284.firebaseio.com",
+  projectId: "train-c0284",
+  storageBucket: "train-c0284.appspot.com",
+  messagingSenderId: "249191595233"
+};
+firebase.initializeApp(config);
+var database = firebase.database();
 
-  var database = firebase.database();
+$('#addTrain').on('click',function(){
+    let trainName = $('#train_name').val().trim();
+    let destination = $('#destination').val().trim();
+    let firstTrain = moment($('#first_train').val().trim(),"HH:mm").format("HH:mm");
+    let frequency = $('#frequency').val().trim();
 
-  $('#addTrain').on('click',function(){
-      let trainName = $('#').val().trim();
-      let location = $('#').val().trim();
-      let firstTrain = moment($('#')).val().trim();
-
-      let newTrain = {
-          name: trainName,
-          place: location,
-          train: firstTrain
+    let newTrain = {
+        name: trainName,
+        place: destination,
+        train: firstTrain,
+        fre: frequency,
       }
+  database.ref().push(newTrain);
+  $("#train_name").val("");
+  $("#destination").val("");
+  $("#first_train").val("");
+  $("#frequency").val("");
+  return false;
+});
 
-      database.ref().push(newTrain);
-  });
 
-  database.ref().on('child_added',function(childSnapshot){
-      let trainName = childSnapshot.val().name;
-      let location = childSnapshot.val().place;
-      
-  })
+database.ref().on('child_added',function(childSnapshot){
+    let trainName = childSnapshot.val().name;
+    let destination = childSnapshot.val().place;
+    let firstTrain = childSnapshot.val().train;
+    let frequency = childSnapshot.val().fre;
+
+    // let convertTime = moment(firstTrain,"HH:mm");
+    // console.log(convertTime)
+    let currentTime = moment().format("HH:mm");
+    let currentMin = moment().format("mm");
+    // console.log(currentMin);
+    
+    // let timeBtwn = moment().diff(moment(convertTime)+' min');
+    let timeRemain = frequency - currentMin % frequency;
+    console.log(timeRemain);
+
+    
+    
+    let minsAwawy = frequency - timeRemain;
+    // let nextTrain = moment().add(minsAwawy+" min").format("HH:mm");
+    $('#schedule_table>tbody').append('<tr><td>'+trainName+"</td><td>"+destination+"</td><td>"+firstTrain+"</td><td>"+currentTime+"</td><td>"+frequency+"</td><td>"+timeRemain+"</td><td>");
+});
